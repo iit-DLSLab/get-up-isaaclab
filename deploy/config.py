fcfa2e1cdf4bc569fe37832e8b72473be9de5d7c
giulioturrisi <giulioturrisi@hotmail.it>
2026-05-13 14:27:58 +0200

    removing stuff

--- deploy/config.py
+++ deploy/config.py
@@ -4,20 +4,11 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path+"/../")
 sys.path.append(dir_path+"/../scripts/rsl_rl")
 
-robot = 'go2'  # 'aliengo', 'go2', 'b2', 'hyqreal2' 
+robot = 'go2'  # 'go2'' 
 scene = 'random_boxes'  # flat, random_boxes, random_pyramids, perlin
 
 # ----------------------------------------------------------------------------------------------------------------
-if(robot == "aliengo"):
-    Kp_walking = 21.5
-    Kd_walking = 3.5
-
-    Kp_stand_up_and_down = 25.
-    Kd_stand_up_and_down = 2.
-
-    policy_folder_path = dir_path + "/../tested_policies/" + robot + "/symmetricactor_data_augmented"
-
-elif(robot == "go2"):
+if(robot == "go2"):
     Kp_walking = 20.0
     Kd_walking = 2.0 #1.5
 
@@ -26,12 +17,6 @@ elif(robot == "go2"):
 
     policy_folder_path = dir_path + "/../tested_policies/" + robot + "/2026-04-08_19-12-57"
 
-elif(robot == "b2"):
-    Kp_walking = 20.
-    Kd_walking = 1.5
-
-    Kp_stand_up_and_down = 25.
-    Kd_stand_up_and_down = 2.
 else:
     raise ValueError(f"Robot {robot} not supported")
 
